bddb1ddfd790bfe11f5d26c0e93fb2a98fca48b0
Manuel <manuramas1@gmail.com>
2024-12-10 20:43:54 +0100

    Retrofit setup

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/response/Cries.kt
@@ -0,0 +1,6 @@
+package com.plcoding.jetpackcomposepokedex.data.remote.response
+
+data class Cries(
+    val latest: String,
+    val legacy: String
+)
---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/repository/PokemonRepository.kt
@@ -0,0 +1,31 @@
+package com.plcoding.jetpackcomposepokedex.repository
+
+import com.plcoding.jetpackcomposepokedex.data.remote.PokeAPI
+import com.plcoding.jetpackcomposepokedex.data.remote.response.Pokemon
+import com.plcoding.jetpackcomposepokedex.data.remote.response.PokemonList
+import com.plcoding.jetpackcomposepokedex.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class PokemonRepository @Inject constructor(
+    private val api: PokeAPI
+) {
+    suspend fun getPokemonList(limit: Int, offset: Int): Resource<PokemonList> {
+        val response = try {
+            api.getPokemonList(limit, offset)
+        } catch (e: Exception) {
+            return Resource.Error("An unknown error occurred.")
+        }
+        return Resource.Success(response)
+    }
+
+    suspend fun getPokemonInfo(pokemonName: String): Resource<Pokemon> {
+        val response = try {
+            api.getPokemonInfo(pokemonName)
+        } catch (e: Exception) {
+            return Resource.Error("An unknown error occurred.")
+        }
+        return Resource.Success(response)
+    }
+}
